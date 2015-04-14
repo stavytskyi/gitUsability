a77d6ae4c09fc4d1f5750c7931115d869845f39a
Maksym Stavytskyi <stavytskyi@gmail.com>
2015-04-14 19:14:58 +0300

    Fix problem long git command with a lot of parameters

--- gitalias.groovy
+++ gitalias.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 'git config --local alias.move checkout'.execute()
-'git config --local alias.unstash stash pop'.execute()
-'gir config --local alias.tree git log --graph --oneline --decorate'.execute()
+['git', 'config', '--local', 'alias.unstash', 'stash pop'].execute()
+['git', 'config', '--local', 'alias.tree', 'log --graph --oneline --decorate'].execute()
