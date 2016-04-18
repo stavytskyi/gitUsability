91a979510906d2657e5daecdc97cbc355b3e7425
Maksym Stavytskyi <stavytskyi@gmail.com>
2016-04-18 09:32:52 +0300

    change local to global properties

--- gitalias.groovy
+++ gitalias.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 'git config --local alias.move checkout'.execute()
-['git', 'config', '--local', 'alias.unstash', 'stash pop'].execute()
-['git', 'config', '--local', 'alias.tree', 'log --graph --oneline --decorate'].execute()
+['git', 'config', '--global', 'alias.unstash', 'stash pop'].execute()
+['git', 'config', '--global', 'alias.tree', 'log --graph --oneline --decorate'].execute()
